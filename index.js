e5c8fbee230ba0edb850b7872b1066c16f4a02f3
Theerapong25 <theerapong.kl@ku.th>
2025-01-27 13:54:08 +0700

    Update

---
+++ index.js
@@ -0,0 +1,27 @@
+function submitData(){
+    let firstNameDOM = document.querySelector('input[name=firstname]')
+    let lastNameDOM = document.querySelector('input[name=lastname]')
+    let ageDOM = document.querySelector('input[name=age]')
+    let genderDOM = document.querySelector('input[name=gender]:checked')
+    let interestsDOMs = document.querySelectorAll('input[name=interests]:checked')
+    let descriptionDOM = document.querySelector('textarea[name=description]')
+    
+    let interests=''
+    for(let i=0;i<interestsDOMs.length;i++){
+        interests+=interestsDOMs[i].value
+        if(i != interestsDOMs.length-1){
+            interests+=','
+        }
+    }
+
+
+    let userData={
+        firstName: firstNameDOM.value,
+        lastName: lastNameDOM.value,
+        age: ageDOM.value,
+        gender: genderDOM.value,
+        descriptionDOM: descriptionDOM.value,
+        interest : interests
+    }
+    console.log("submitData",userData);
+}
